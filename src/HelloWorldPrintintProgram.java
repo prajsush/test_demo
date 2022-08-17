0731f97b39bd3d16d48798cedd1ae92cd741acaf
prajsush <prajakta950371@gmail.com>
2022-08-17 15:34:48 +0530

    commiteddone..

---
+++ src/HelloWorldPrintintProgram.java
@@ -0,0 +1,11 @@
+
+/* Assighment No 1
+ * Program Name-Write a java Program to print Velocity Corporate Training
+ * */
+public class HelloWorldPrintintProgram {
+	
+	public static void main(String argd[])
+	{
+       System.out.println("Velocity Corporate training Center.");
+}
+}
